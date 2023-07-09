a425743cd44eeda46b886ab34eeddf63becfa153
iorphx <iorphx@gmail.com>
2023-07-10 08:41:11 +0900

    eslint and prettier change

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:solid/typescript',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:solid/typescript', 'plugin:prettier/recommended'],
   overrides: [
     {
       env: {
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   semi: true,
   tabWidth: 2,
   trailingComma: 'all',
-  printWidth: 120,
+  printWidth: 160,
   arrowParens: 'avoid',
   endOfLine: 'auto',
   plugins: [require('prettier-plugin-tailwindcss')],
