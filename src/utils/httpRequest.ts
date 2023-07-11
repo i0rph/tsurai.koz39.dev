390c20e2753361dc60830dfd91f0f130f66f29fb
iorphx <iorphx@gmail.com>
2023-07-11 22:04:19 +0900

    change api base url

--- src/utils/httpRequest.ts
+++ src/utils/httpRequest.ts
@@ -22,7 +22,7 @@ async function request(object: IObject, requestOption = {}) {
     const option = setDefaultRequestOption(requestOption);
 
     const response = await axios.create({
-      baseURL: import.meta.env.VITE_API_BASEURL ?? 'https://api.koz39.dev/',
+      baseURL: import.meta.env.VITE_API_BASEURL ?? 'https://puyo.koz39.dev/',
       headers: {
         'Content-type': option.type ?? 'application/json',
       },
